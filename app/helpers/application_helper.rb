bd3495dea2fcac5b36fae44ced9b824964e3d7a0
kenta-crv <adp@crold-valieat.jp>
2020-08-25 23:54:56 +0900

    fiest

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: "介護施設・老人ホームを知るなら｜介護の里",
-      title: "<%= yield(:title) || eatery-japan %>" ,
-      description: "介護施設・老人ホームを知るなら介護の里",
-      keywords:    "介護施設,老人ホーム",
+      site: "エンディングから葬儀まで知るなら｜トトノエ",
+      title: "<%= yield(:title) || トトノエ %>" ,
+      description: "エンディングから葬儀まで知るならトトノエ",
+      keywords:    "葬儀,エンディング",
       canonical: request.original_url,  # 優先されるurl
       charset: "UTF-8",
       #icon: [href: image_url('favicon.ico')]
